8b08ab09de4df0830011b10f0a79c56904697999
baz00ka <39489282+SafalKoirala@users.noreply.github.com>
2020-01-27 21:08:55 +0545

    keeping score

--- randomnum.py
+++ randomnum.py
@@ -2,6 +2,7 @@ import random
 
 
 try_again ="yes"
+score=0
         
 while try_again=="yes" or try_again =="y":
     x=random.randint(1,99)
@@ -9,14 +10,19 @@ while try_again=="yes" or try_again =="y":
 
     if(y==x):
          print('CORRECT GUESS')
+         score=score+1
     elif(y<x):
         print('GUESSED TOO LOW')
 
     else:
          print('TOO HIGH')
-
     print("ACTUAL OUTPUT",x)
+    print("TOTAL SCORE: ",score)
+         
+    
+   
         
     try_again = input("PRESS y OR yes to try again??\n PRESS any key to exit: ")
+   
         
-    
+   
